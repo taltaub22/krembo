650d5772d5a89c2812505127e2f4b47cb58e5cc4
Tal Taub <taltaub22@gmail.com>
2017-02-24 20:03:48 +0200

    Changes for heruko

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: python krembo.py
-
+web: python dealscraper/krembo.py
--- krembo.py
+++ krembo.py
@@ -1,27 +1,6 @@
-from __future__ import unicode_literals
+import os
 from flask import Flask
 from flask import render_template
-import multiprocessing
-import gunicorn.app.base
-from gunicorn.six import iteritems
-
-
-def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
-
-
-def handler_app(environ, start_response):
-    response_body = b'Works fine'
-    status = '200 OK'
-
-    response_headers = [
-        ('Content-Type', 'text/plain'),
-    ]
-
-    start_response(status, response_headers)
-
-    return [response_body]
-
 
 app = Flask(__name__)
 
@@ -31,26 +10,6 @@ def index(title="Home"):
     return render_template("MainTamplate.html", title=title)
 
 
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
-
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super(StandaloneApplication, self).__init__()
-
-    def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
 if __name__ == '__main__':
-    options = {
-        'bind': '%s:%s' % ('127.0.0.1', '8080'),
-        'workers': number_of_workers(),
-    }
-    StandaloneApplication(handler_app, options).run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
